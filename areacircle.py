bf7ea5ba99f03c466129729e2e91b46aa43d6403
seasoon <95148374+spacetime-continuum@users.noreply.github.com>
2025-11-19 17:38:30 +0000

    expressionsolver

--- areacircle.py
+++ areacircle.py
@@ -3,4 +3,3 @@ radius = int(input("What is the radius of the circle? "))
 answer = math.pi * radius ** 2
 result = str(round(answer))
 print("The area of the circle is about " + result + " units squared.")
-11

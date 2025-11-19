424a4d8b0cc96b5a05024d0b93065d0edb71c654
seasoon <95148374+spacetime-continuum@users.noreply.github.com>
2025-11-19 17:53:26 +0000

    ascii

--- expressionsolver.py
+++ expressionsolver.py
@@ -1,4 +1,4 @@
 avar = int(input("What is the value of variable a? "))
 bvar = int(input("What is the value of variable b? "))
 answer = (avar - bvar) + (avar * bvar)
-print("" + answer)
+print("The result of (a - b) + (a * b) is " + str(answer) + ".")

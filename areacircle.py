c64c31acece0d46757b5b963c5099da9588979b6
seasoon <95148374+spacetime-continuum@users.noreply.github.com>
2025-11-19 17:23:46 +0000

    refined input and output

--- areacircle.py
+++ areacircle.py
@@ -1,5 +1,6 @@
 import math
-radius = int(input("What is the radius? "))
-result = math.pi * radius ** 2
-print(result)
-
+radius = int(input("What is the radius of the circle? "))
+answer = math.pi * radius ** 2
+result = str(round(answer))
+print("The area of the circle is about " + result + " units squared.")
+11

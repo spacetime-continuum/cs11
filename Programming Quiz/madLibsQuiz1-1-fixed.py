69a62d4567efb1766f5115ca7080a7fdf6612726
seasoon <95148374+spacetime-continuum@users.noreply.github.com>
2025-11-25 01:33:44 +0000

    10/10 errors found

--- Programming Quiz/madLibsQuiz1-1-fixed.py
+++ Programming Quiz/madLibsQuiz1-1-fixed.py
@@ -12,29 +12,27 @@ print('''MadLibs is a fill-in-the-blanks story
 game. The player must choose words based on the 
 given prompts, and the computer will return a 
 short story that includes the words the user chose.''')
-#error 2 add print
+#error 2 and 3 add print and '
 
 play = input("Do you want to play MadLibs? (y/n) ")
-#error 3 mispell
+#error 4 mispell
 
-if play == "y": #error 4 and 5 double == and :
+if play == "y": #error 5 and 6 double == and :
     person_name = input("Choose a name for a person: ")
     place = input("Choose a place: ")
     noun_1 = input("Choose a singlular noun: ")
-    animal_1 = input("Choose an animal: ") #error 6 missing quotes
+    animal_1 = input("Choose an animal: ") #error 7 missing quotes
     adjective_1 = input("Choose an adjective for a feeling: ")
     adjective_2 = input("Choose an adjective: ")
-    adjective_3 = input("Choose an adjective: ") # error 7 missing =
+    adjective_3 = input("Choose an adjective: ") # error 8 missing =
     animal_2 = input("Choose an animal: ")
     food = input("Choose a food: ")
     
-    #error 8 place concatenated wrong
-    print("Over break I am going camping with " + person_name + '''. It is important to be prepared when camping at ''' + place +1 ''', so I made sure to pack a 
-    sleeping bag, flashlight, and a ''' + noun_1 + """. The possibility of seeing a 
-    """ + animal_1 +""" makes me feel """ + adjective_1 +""". I am excited to go 
+    #error 9 place concatenated wrong
+    print("Over break I am going camping with " + person_name + '''. It is important to be prepared when camping at ''' + place + ''', so I made sure to pack a 
+    sleeping bag, flashlight, and a ''' + noun_1 + ". The possibility of seeing a " + animal_1 +" makes me feel " + adjective_1 +""". I am excited to go 
     hiking on the """ + adjective_2 +" trail. If I see a " + adjective_3 + " " + animal_2 +"""
-    on the hike, I will take it home as my new pet! The best part of 
-    camping is eating """ + food + """ by the campfire!
+    on the hike, I will take it home as my new pet! The best part of camping is eating """ + food + """ by the campfire!
     """)
     
     print("Thanks for playing! Goodbye!")
